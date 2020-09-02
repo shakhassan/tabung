ab1179bac6218795eb4a6db89be8b01d2620f6dd
shah <shah@dapat.com>
2020-09-02 23:16:31 +0800

    basic Spring Boot structure

---
+++ src/main/java/org/tabung/main/TabungApplication.java
@@ -0,0 +1,13 @@
+package org.tabung.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tabung/main/TabungApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tabung.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
